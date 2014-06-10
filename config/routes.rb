6519656db7245aaec55706d8f572c3d33e319eed
Anand Tripathi <anand.tripathi@clariontechnologies.co.in>
2014-06-10 16:19:50 +0530

    complete user model creation with devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MyFirstTest::Application.routes.draw do
+  devise_for :users
+
   root :to => 'profiles#welcome'
   resources :profiles
   # The priority is based upon order of creation:
--- db/migrate/20140610101215_create_profiles.rb
+++ db/migrate/20140610101215_create_profiles.rb
@@ -1,7 +1,9 @@
 class CreateProfiles < ActiveRecord::Migration
   def change
     create_table :profiles do |t|
-
+      t.belongs_to :user
+      t.string :address
+      t.string :phone_number
       t.timestamps
     end
   end
